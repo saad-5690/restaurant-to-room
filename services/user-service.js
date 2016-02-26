413d5429fed7ed1b6d2c79c4b5f97be55d02dab6
Muhammad Saad Mateen <saad_5690@hotmail.com>
2016-02-26 05:21:19 +0000

    Implemented strong password hashing with bcrypt

--- services/user-service.js
+++ services/user-service.js
@@ -1,21 +1,27 @@
+var bcrypt = require("bcrypt");
 var User = require("../models/user").User;
 
 exports.addUser = function(user, next){
-  var newUser = new User({
-      firstName: user.firstName,
-      lastName: user.lastName,
-      roomNumber: user.roomNumber,
-      email: user.email.toLowerCase(),
-      password: user.password
-  });
   
-  newUser.save(function(err){
-     if(err){
-         return next(err);
-     } 
-     next(null);
-  });
-  
+  bcrypt.hash(user.password, 10, function(err, hash) {
+    if(err){
+      return next(err);
+    }
+    var newUser = new User({
+        firstName: user.firstName,
+        lastName: user.lastName,
+        roomNumber: user.roomNumber,
+        email: user.email.toLowerCase(),
+        password: hash
+    });
+    
+    newUser.save(function(err){
+       if(err){
+           return next(err);
+       } 
+       next(null);
+    });
+  });  
 };
 
 exports.findUser = function(email, next){
